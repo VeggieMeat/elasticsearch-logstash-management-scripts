716a01372d9674f152ced9b04bb2879e3abda4d3
Brian Altenhofel <brian.altenhofel@vmdoh.com>
2013-07-28 01:23:14 -0500

    add cleanup script

---
+++ cleanup.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+HOST="http://127.0.0.1:9200"
+LIMIT=30
+PREFIX="logstash"
+
+while getopts ":h:l:p" flag
+do
+  case "$flag" in
+    h)
+      HOST=$OPTARG
+      ;;
+    l)
+      LIMIT=$OPTARG
+      ;;
+    p)
+      PREFIX=$OPTARG
+      ;;
+  esac
+done
+
+ESDATA=`curl -s -S "$HOST/_status?pretty=true" | grep $PREFIX | grep -v "index" | awk -F \" {'print $2'} | sort -r`
+
+if [ -z "$ESDATA" ]; then
+  echo "No '$PREFIX' indexes returned from $HOST."
+  exit 1
+fi
+
+declare -a INDEXES=($ESDATA)
+
+if [ ${#INDEXES[@]} -gt $LIMIT ]; then
+  for index in ${INDEXES[@]:$LIMIT}; do
+    if [ -n "$INDEXES" ]; then
+      echo `date` " -- Deleting index: $index."
+      ACK=`curl -s -S -XDELETE "$HOST/$index/" `
+      if [ "$ACK" == "ok:true acknowledged:true" ]; then
+        echo "-- $index acknowledgement: $ACK"
+      else
+        echo "-- $index acknowledgement: $ACK ---FAILED---"
+        exit 1
+      fi
+    fi
+  done
+fi
+
+exit 0
